a93dee0075196b06e66a5f5d3751456ba45c3348
gmorell <gmp@gmp.io>
2023-02-06 00:28:50 -0500

    DOTA TWO BAYBEE

--- lib/lambent_ex/machine/ext/gsi.ex
+++ lib/lambent_ex/machine/ext/gsi.ex
@@ -108,6 +108,10 @@ defmodule LambentEx.Machine.Ext.GSI do
     []
   end
 
+  def generate_flash(state, color, ticks, value) do
+    []
+  end
+
   defp read(:csgo_team, state), do: state[:gs] |> get_in(["player", "team"])
   defp read(:csgo_health, state), do: state[:gs] |> get_in(["player", "state", "health"]) || 0
   defp read(:csgo_armor, state), do: state[:gs] |> get_in(["player", "state", "armor"]) || 0
@@ -200,14 +204,102 @@ defmodule LambentEx.Machine.Ext.GSI do
     }
   end
 
+  # game state
+  defp read(:dota_map_day, state), do: state[:gs] |> get_in(["map", "daytime"]) || false
+  defp read(:dota_clock_time, state), do: state[:gs] |> get_in(["map", "clock_time"]) || 0
+  defp read(:dota_game_time, state), do: state[:gs] |> get_in(["map", "game_time"]) || 0
+  defp read(:dota_score_r, state), do: state[:gs] |> get_in(["map", "radiant_score"]) || 0
+  defp read(:dota_score_d, state), do: state[:gs] |> get_in(["map", "dire_score"]) || 0
+  defp read(:dota_paused, state), do: state[:gs] |> get_in(["map", "paused"]) || false
+  defp read(:dota_team, state), do: state[:gs] |> get_in(["player", "team_name"])
+  defp read(:dota_ward_purchase_cooldown, state), do: state[:gs] |> get_in(["map", "ward_purchase_cooldown"])
+  # scoring
+  defp read(:dota_player_kills, state), do: state[:gs] |> get_in(["player", "kills"]) || 0
+  defp read(:dota_player_deaths, state), do: state[:gs] |> get_in(["player", "deaths"]) || 0
+  defp read(:dota_player_assists, state), do: state[:gs] |> get_in(["player", "assists"]) || 0
+  defp read(:dota_player_denies, state), do: state[:gs] |> get_in(["player", "denies"]) || 0
+  defp read(:dota_player_last_hits, state), do: state[:gs] |> get_in(["player", "last_hits"]) || 0
+  defp read(:dota_player_kill_streak, state), do: state[:gs] |> get_in(["player", "kill_streak"]) || 0
+  # gold
+  defp read(:dota_player_xpm, state), do: state[:gs] |> get_in(["player", "xpm"]) || 0
+  defp read(:dota_player_gpm, state), do: state[:gs] |> get_in(["player", "gpm"]) || 0
+  defp read(:dota_player_gold, state), do: state[:gs] |> get_in(["player", "gold"]) || 0
+  defp read(:dota_player_gold_unreliable, state), do: state[:gs] |> get_in(["player", "gold_unreliable"]) || 0
+  defp read(:dota_player_gold_reliable, state), do: state[:gs] |> get_in(["player", "gold_reliable"]) || 0
+  defp read(:dota_player_gold_from_creep_kills, state), do: state[:gs] |> get_in(["player", "gold_from_creep_kills"]) || 0
+  defp read(:dota_player_gold_from_hero_kills, state), do: state[:gs] |> get_in(["player", "gold_from_hero_kills"]) || 0
+  defp read(:dota_player_gold_from_income, state), do: state[:gs] |> get_in(["player", "gold_from_income"]) || 0
+  defp read(:dota_player_gold_from_shared, state), do: state[:gs] |> get_in(["player", "gold_from_shared"]) || 0
+  # numbers
+  defp read(:dota_mana, state), do: state[:gs] |> get_in(["hero", "mana"]) || 0
+  defp read(:dota_mana_max, state), do: state[:gs] |> get_in(["hero", "max_mana"]) || 0
+  defp read(:dota_mana_pct, state), do: state[:gs] |> get_in(["hero", "mana_percent"]) || 0
+  defp read(:dota_health, state), do: state[:gs] |> get_in(["hero", "health"]) || 0
+  defp read(:dota_health_max, state), do: state[:gs] |> get_in(["hero", "max_health"]) || 0
+  defp read(:dota_health_pct, state), do: state[:gs] |> get_in(["hero", "health_percent"]) || 0
+  # statuses
+  defp read(:dota_hero_smoked, state), do: state[:gs] |> get_in(["hero", "smoked"]) ||false
+  defp read(:dota_hero_alive, state), do: state[:gs] |> get_in(["hero", "alive"]) || false
+  defp read(:dota_hero_muted, state), do: state[:gs] |> get_in(["hero", "muted"]) || false
+  defp read(:dota_hero_break, state), do: state[:gs] |> get_in(["hero", "break"]) || false
+  defp read(:dota_hero_hexed, state), do: state[:gs] |> get_in(["hero", "hexed"]) || false
+  defp read(:dota_hero_silenced, state), do: state[:gs] |> get_in(["hero", "silenced"]) || false
+  defp read(:dota_hero_has_debuff, state), do: state[:gs] |> get_in(["hero", "has_debuff"]) || false
+  defp read(:dota_hero_disarmed, state), do: state[:gs] |> get_in(["hero", "disarmed"]) || false
+  defp read(:dota_hero_stunned, state), do: state[:gs] |> get_in(["hero", "stunned"]) || false
+  defp read(:dota_hero_magicimmune, state), do: state[:gs] |> get_in(["hero", "magicimmune"]) || false
+  # hero
+  defp read(:dota_hero_xpos, state), do: state[:gs] |> get_in(["map", "xpos"]) || 0
+  defp read(:dota_hero_ypos, state), do: state[:gs] |> get_in(["map", "ypos"]) || 0
+  defp read(:dota_hero_level, state), do: state[:gs] |> get_in(["hero", "level"]) || 0
+  defp read(:dota_hero_xp, state), do: state[:gs] |> get_in(["hero", "xp"]) || 0
+  defp read(:dota_hero_attributes_level, state), do: state[:gs] |> get_in(["hero", "attributes_level"]) || 0
+  defp read(:dota_hero_respawn_seconds, state), do: state[:gs] |> get_in(["hero", "respawn_seconds"]) || 0
+  defp read(:dota_hero_buyback_money, state), do: state[:gs] |> get_in(["hero", "buyback_cost"]) || 0
+  defp read(:dota_hero_buyback_seconds, state), do: state[:gs] |> get_in(["hero", "buyback_cooldown"]) || 0
+  # talents
+  defp read(:dota_hero_aghs_scepter, state), do: state[:gs] |> get_in(["hero", "aghanims_scepter"]) || false
+  defp read(:dota_hero_aghs_shard, state), do: state[:gs] |> get_in(["hero", "aghanims_shard"]) || false
+  defp read(:dota_hero_talent_1, state), do: state[:gs] |> get_in(["hero", "talent_1"]) || false
+  defp read(:dota_hero_talent_2, state), do: state[:gs] |> get_in(["hero", "talent_2"]) || false
+  defp read(:dota_hero_talent_3, state), do: state[:gs] |> get_in(["hero", "talent_3"]) || false
+  defp read(:dota_hero_talent_4, state), do: state[:gs] |> get_in(["hero", "talent_4"]) || false
+  defp read(:dota_hero_talent_5, state), do: state[:gs] |> get_in(["hero", "talent_5"]) || false
+  defp read(:dota_hero_talent_6, state), do: state[:gs] |> get_in(["hero", "talent_6"]) || false
+  defp read(:dota_hero_talent_7, state), do: state[:gs] |> get_in(["hero", "talent_7"]) || false
+  defp read(:dota_hero_talent_8, state), do: state[:gs] |> get_in(["hero", "talent_8"]) || false
+  # abilities
+  defp read(:dota_hero_ability_1, state), do: state[:gs] |> get_in(["hero", "ability_1"]) || false
+  defp read(:dota_hero_ability_2, state), do: state[:gs] |> get_in(["hero", "ability_2"]) || false
+  defp read(:dota_hero_ability_3, state), do: state[:gs] |> get_in(["hero", "ability_3"]) || false
+  defp read(:dota_hero_ability_4, state), do: state[:gs] |> get_in(["hero", "ability_4"]) || false
+  defp read(:dota_hero_ability_5, state), do: state[:gs] |> get_in(["hero", "ability_5"]) || false
+  defp read(:dota_hero_ability_6, state), do: state[:gs] |> get_in(["hero", "ability_6"]) || false
+  defp read(:dota_hero_ability_7, state), do: state[:gs] |> get_in(["hero", "ability_7"]) || false
+  defp read(:dota_hero_ability_8, state), do: state[:gs] |> get_in(["hero", "ability_8"]) || false
+  ## these are passed in the result of above eg; read(:dota_hero_ability_active, read(:dota_hero_ability_1, state))
+  defp read(:dota_hero_ability_active, state), do: state |> Map.get("ability_active") || false
+  defp read(:dota_hero_ability_can_cast, state), do: state |> Map.get("can_cast") || false
+  defp read(:dota_hero_ability_charge_cooldown, state), do: state |> Map.get("charge_cooldown") || 0
+  defp read(:dota_hero_ability_charges, state), do: state |> Map.get("charges") || 0
+  defp read(:dota_hero_ability_cooldown, state), do: state |> Map.get("cooldown") || 0
+  defp read(:dota_hero_ability_level, state), do: state |> Map.get("level") || 0
+  defp read(:dota_hero_ability_max_charges, state), do: state |> Map.get("max_charges") || 0
+  defp read(:dota_hero_ability_name, state), do: state |> Map.get("name")
+  defp read(:dota_hero_ability_passive, state), do: state |> Map.get("passive") || false
+  defp read(:dota_hero_ability_ultimage, state), do: state |> Map.get("ultimate") || false
+  # building statuses # todo
+  # items # todo
+  # wearables # todo
+
   defp read(:dota, state) do
     %{
-      health_fill: [],
+      health_fill: generate_fill(state, {@green, @whitish}, read(:dota_health, state), read(:dota_health_max, state)),
       health_bright: [],
-      mana_fill: [],
+      mana_fill: generate_fill(state, {@blue, @whitish}, read(:dota_mana, state), read(:dota_mana_max, state)),
       mana_bright: [],
-      money_fill: [],
-      money_bright: []
+      money_reliable_fill: generate_fill(state, {@yellow, @whitish}, read(:dota_player_gold_reliable, state), read(:dota_player_gold, state)),
+      money_reiiable_bright: []
     }
   end
 
@@ -234,10 +326,7 @@ defmodule LambentEx.Machine.Ext.GSI do
   end
 
   def handle_info({name, path, data}, state) do
-    IO.inspect({name, state[:name]})
-
     if name == state[:name] do
-      #      IO.inspect(data)
       flashes = delta(data, state)
       {:noreply, state |> Map.put(:path, path) |> Map.put(:gs, data)}
     else
